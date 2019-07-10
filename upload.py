3c4cbf58303ecbe28734dec8c7a89c517e857f7e
Antony.J.Malakkaran <35623607+antonyjm462@users.noreply.github.com>
2019-07-10 18:45:50 +0530

    Create upload.py

---
+++ upload.py
@@ -0,0 +1,7 @@
+from google.colab import files
+def getfiles():
+    _files = files.upload()
+    if len(_files) >0:
+       for k,v in _files.items():
+         open(k,'wb').write(v)
+getLocalFiles()
